a834bee6e5edbdb2ef240b01eee4863cd3e508b4
Ashish Baghudana <ashish.baghudana26@gmail.com>
2018-12-28 20:44:08 +0530

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='tess',
+    version='0.1.0',
+    description='The funniest joke in the world',
+    url='http://github.com/ashishbaghudana/tess',
+    author='Ashish Baghudana',
+    author_email='ashish@ashishb.me',
+    license='MIT',
+    packages=['tess'],
+    zip_safe=False)
